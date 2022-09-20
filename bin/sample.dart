b56a3f5dd0b87948c0fd4ca037ad94b8a5311a7f
ACER <muhammednihal8454@gmail.com>
2022-09-20 02:03:19 -0700

    commit

--- bin/sample.dart
+++ bin/sample.dart
@@ -1,5 +1,22 @@
 import 'package:sample/sample.dart' as sample;
+import 'dart:io';
 
 void main(List<String> arguments) {
+  String name="nihal";
+  int age=40;
+  double mark=6.5;
+  String email="nihal123@gmail.com";
+  int a=10;
+  int b=5;
+  String s="sum";
+  String d="difference";
+  String m="multiplication";
   print('Hello world: ${sample.calculate()}!');
+   print("my name is $name");
+  print("im $age years old");
+  print("my mark is $mark");
+  stdout.write("email is $email");
+  print("\n$s=${a+b}");
+  print("\n$d=${a-b}");
+  print("\n$m=${a*b}");
 }
